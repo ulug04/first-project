ba37369687e80dc3f43b277bb778dc763b0eeb47
ulug04 <fayzulug7@gmail.com>
2022-09-16 23:36:27 -0700

    new file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,41 @@ fun main(args: Array<String>) {
 
 
     // 3 cars
-    Car().drive()
-    Car().stop()
-    Car().drive()
+//    Car().drive()
+//    Car().stop()
+//    Car().drive()
 
 
 
 
     // one car
-    val bmw = Car()
+    //val bmw = Car()
 
-    bmw.drive()
-    bmw.stop()
-    bmw.drive()
+//    bmw.drive()
+//    bmw.stop()
+//    bmw.drive()
 
 
-    println()
+    val a = 5
+    val b = 10
+    var c = 0
+
+    c = a + b
+
+
+    println(c)
+
+    c = (a * b)/2
+
+    println(c)
+
+    c -= 5  // c = c - 5
+
+    c += 5 // c = c + 5
+
+    c = b.plus(a)
+
+    println (c)
 }
 
 
