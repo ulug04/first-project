210355a8d58b495138d716984173b10c4a8ab7f2
ulug04 <fayzulug7@gmail.com>
2022-09-24 06:15:18 -0700

    file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,19 @@
 fun main(args: Array<String>) {
 
-    val c = mutableListOf(67, 45, 3, 50) // 0, 1 , 2, 3
+//    val c = mutableListOf(67, 45, 3, 50) // 0, 1 , 2, 3
+//
+//    val d = mutableListOf(1, 2, 3, 4)
+//
+//    var number = d[1]
+//
+//    c.forEach {ggg ->
+//
+//    number = number * ggg
+//
+//        println(number)
+//
+//    }
 
-    val d = mutableListOf(1, 2, 3, 4)
-
-    var number = d[1]
-
-    c.forEach {ggg ->
-
-    number = number * ggg
-
-        println(number)
-
-    }
 
 
 
